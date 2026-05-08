502624565ed6ae798db4d0a7b4776cde327ec5f4
Michael Obster <95032975+mobster80@users.noreply.github.com>
2026-05-08 13:33:32 +0200

    Update patch version from r44 to r45

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const packageJson = require('./package.json');
 // const adapterName = packageJson.name.split('.').pop();
 const adapterVersion = packageJson.version;
 
-const patchVersion = 'r44';
+const patchVersion = 'r45';
 
 let adapter;
 let deviceIpAdress;
